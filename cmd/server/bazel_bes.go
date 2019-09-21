33657e9d6630fa87c063b79f71b7be8bb836a012
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2019-09-21 10:56:32 -0400

    Add handlers for specific build events

--- cmd/server/bazel_bes.go
+++ cmd/server/bazel_bes.go
@@ -24,6 +24,52 @@ func (s *server) PublishLifecycleEvent(ctx context.Context, req *pb.PublishLifec
 	return &pb_empty.Empty{}, nil
 }
 
+func (s *server) processCompleted(id *bazel_pb.BuildEventId_TargetCompletedId, c *bazel_pb.TargetComplete) error {
+	return nil
+}
+
+func (s *server) processAction(id *bazel_pb.BuildEventId_ActionCompletedId, a *bazel_pb.ActionExecuted) error {
+	return nil
+}
+
+func (s *server) processTestResult(id *bazel_pb.BuildEventId_TestResultId, r *bazel_pb.TestResult) error {
+	return nil
+}
+
+func (s *server) processTestSummary(id *bazel_pb.BuildEventId_TestSummaryId, ts *bazel_pb.TestSummary) error {
+	return nil
+}
+
+func (s *server) processBuildFinished(f *bazel_pb.BuildFinished) error {
+	return nil
+}
+
+func (s *server) processBuildAborted(a *bazel_pb.Aborted) error {
+	return nil
+}
+
+func (s *server) processBuildEvent(be *bazel_pb.BuildEvent) error {
+	if c := be.GetCompleted(); c != nil {
+		return s.processCompleted(be.GetId().GetTargetCompleted(), c)
+	}
+	if a := be.GetAction(); a != nil {
+		return s.processAction(be.GetId().GetActionCompleted(), a)
+	}
+	if r := be.GetTestResult(); r != nil {
+		return s.processTestResult(be.GetId().GetTestResult(), r)
+	}
+	if ts := be.GetTestSummary(); ts != nil {
+		return s.processTestSummary(be.GetId().GetTestSummary(), ts)
+	}
+	if f := be.GetFinished(); f != nil {
+		return s.processBuildFinished(f)
+	}
+	if a := be.GetAborted(); a != nil {
+		return s.processBuildAborted(a)
+	}
+	return nil
+}
+
 func (s *server) PublishBuildToolEventStream(stream pb.PublishBuildEvent_PublishBuildToolEventStreamServer) error {
 	log.Println("Reading build tool event stream")
 	for {
@@ -39,9 +85,11 @@ func (s *server) PublishBuildToolEventStream(stream pb.PublishBuildEvent_Publish
 		case *pb.BuildEvent_BazelEvent:
 			be := &bazel_pb.BuildEvent{}
 			if err := pb_ptypes.UnmarshalAny(e.BazelEvent, be); err != nil {
-				log.Printf("ERROR: Unable to unmarshall Bazel BuildEvent from BazelEvent: %v\n", err)
-			} else {
-				log.Println("Got Bazel BuildEvent", be.GetId())
+				log.Printf("ERROR: Unable to unmarshall Bazel BuildEvent from BazelEvent: %v", err)
+				break
+			}
+			if err := s.processBuildEvent(be); err != nil {
+				log.Printf("ERROR: Unable to process Bazel BuildEvent: %v", err)
 			}
 		}
 		resp := &pb.PublishBuildToolEventStreamResponse{StreamId: req.OrderedBuildEvent.StreamId, SequenceNumber: req.OrderedBuildEvent.SequenceNumber}
